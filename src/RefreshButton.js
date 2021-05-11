47c9c24a6164ba592d0d67ecb04c34085213f8f7
arielmerin <arielmerino@ciencias.unam.mx>
2021-05-10 19:59:51 -0500

    finit

--- src/RefreshButton.js
+++ src/RefreshButton.js
@@ -2,7 +2,6 @@ import "./assets/css/styles.css"
 
 
 const RefreshButton = ({cambiarFrase}) =>{
-    const refreshPage = ()=> window.location.reload();
     return(
         <button onClick={cambiarFrase} className="button ">
             Otra frase
